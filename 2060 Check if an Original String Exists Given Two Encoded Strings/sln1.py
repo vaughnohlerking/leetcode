746065078c2cf252d243414427888398a7017dc0
Vaughn Ohlerking <vaughnohlerking@gmail.com>
2023-11-30 23:04:47 -0600

    Create sln1.py

---
+++ 2060 Check if an Original String Exists Given Two Encoded Strings/sln1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        str1 = ""
+        
+        for word in word1:
+            str1 += word
+        str2 = ""
+        for word in word2:
+            str2 += word
+        if str1 == str2:
+            return True
+        else:
+            return False
+            
