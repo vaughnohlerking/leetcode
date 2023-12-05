bddd16653da7ea509dcc9e4d1cdee2ecead362b7
Vaughn Ohlerking <vaughnohlerking@gmail.com>
2023-12-04 18:20:10 -0600

    Create soln2.py

---
+++ 1688. Count of Matches in Tournament/soln2.py
@@ -0,0 +1,13 @@
+# beginning to lose faith in LeetCode's assesment of runtime. soln1.py is a much slower solution but tested substantially faster on the website
+
+# Runtime
+# 28ms
+# Beats 96.43%of users with Python3
+
+# Memory
+# 16.36MB
+# Beats 5.91%of users with Python3
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
